0667e191e842a2c44b1c87363ad60750b9a9d77b
HeoEunJae <dmswo159@gmail.com>
2022-10-12 21:12:45 +0900

    백그라운드테스트

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+  const backgroundImgURL ="https://cdn-lostark.game.onstove.com/2018/obt/assets/images/pc/bg/bg_index_profile.jpg?4767f65c7f7b4b14626ad00f0a23c101";
+  return (
+    <div className='' style={{ backgroundImage: `url(${backgroundImgURL})`, height:"1000px" }}>
+        
+    </div>
+  );
+};
+
+export default Main;
